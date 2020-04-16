9eaaf767c73e6666271d3c6565ad6c83329af0ee
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-16 02:15:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,4 +28,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.detect do |planeteer|
     planeteer == valid_calls
+  end
 end
