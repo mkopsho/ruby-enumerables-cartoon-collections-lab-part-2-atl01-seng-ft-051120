954d5bb07440964c67e9d35322d6dedb9fa9f893
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-16 02:29:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,11 +26,6 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
-  planeteer_calls.detect do |planeteer| # I dislike this because it doesn't feel DRY, but it works. Anything better?
-    planeteer == "Earth!" ||
-    planeteer == "Wind!" ||
-    planeteer == "Fire!" ||
-    planeteer == "Water!" ||
-    planeteer == "Heart!"
+  
   end
 end
